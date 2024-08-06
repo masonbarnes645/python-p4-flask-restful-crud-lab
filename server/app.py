1d65a8f496206f1f81cf9ce3fb6b4e69a2f29d4b
Mason Barnes <barnesmason980@gmail.com>
2024-08-06 16:45:38 -0700

    Lab Complete

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from sqlalchemy_serializer import SerializerMixin
 
 from models import db, Plant
 
@@ -50,6 +51,52 @@ class PlantByID(Resource):
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
+
+class EditPlant(Resource, SerializerMixin):
+
+    def patch(self, id):
+        data = request.get_json()
+        plant = db.session.get(Plant, id)
+
+
+        if not plant:
+            return {'message': 'Plant not found'}, 404
+
+        if not data:
+            return {'message': 'No data provided'}, 400
+        try:
+            
+            plant.name = data.get('name', plant.name)
+            plant.image = data.get('image', plant.image)
+            plant.price = data.get('price', plant.price)
+            plant.is_in_stock = data.get('is_in_stock', plant.is_in_stock)
+
+            db.session.commit()
+            return plant.to_dict(), 200
+        except Exception as e:
+            db.session.rollback()
+            return {'Error'}
+
+
+api.add_resource(EditPlant, '/plants/<int:id>')
+
+
+
+class DeletePlant(Resource):
+    def delete(self, id):
+        plant = db.session.get(Plant, id)
+
+        if not plant:
+            return TypeError, 404
+            
+        try:
+            db.session.delete(plant)
+            db.session.commit()
+            return make_response('', 204)
+        except:
+            db.session.rollback()
+
+api.add_resource(DeletePlant, '/plants/<int:id>' )
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
